a0fd1139fea6d242b46be5079543b96b2b4e0b48
Juan Figuera <juanfiguera@gmail.com>
2013-04-26 19:10:35 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EntouraGe::Application.config.secret_token = '71e342d6f17ba57e17d92877ff8bd840f42faf1ae6fe1ad7d2e97bfe35a152ade71cb906298d0f4474ab091fde101ad1f820212b80e56ac95e203ebff35f1fe3'
